c9d282da6d905a6151119514791ed224889d4e3d
Andrew Lee <acelee500@gmail.com>
2022-12-09 18:37:47 -0500

    Completed Work Scene

--- GroupGameProject/Assets/Scripts/RandomScene.cs
+++ GroupGameProject/Assets/Scripts/RandomScene.cs
@@ -10,7 +10,7 @@ public class RandomScene : MonoBehaviour
     [SerializeField] private AnimationCurve animCurve;
     [SerializeField] private TextMeshProUGUI scenarioText;
     [SerializeField] public string[] scenarios = new string[] { "Your manager asked if you would be willing to take on an extra shift. You know you really have to study, but the extra money would be nice.", 
-    "Your manager has offered you a promotion, but it does require you to take on an extra shift a week.", "An angry customer is insulting you and the spotlight is now on you."};
+    "Your coworker just quit and your team is short staffed, the manager asks if you could work longer shifts", "An angry customer is insulting you and the spotlight is now on you."};
 
     [SerializeField] GameObject scene1;
     [SerializeField] GameObject scene2;
@@ -63,11 +63,6 @@ public class RandomScene : MonoBehaviour
     {
         if ((int)number == 0)
         {
-            // buttonYes = GameObject.Find("Scenario1Yes").GetComponent<Button>();
-            // buttonNo = GameObject.Find("Scenario1No").GetComponent<Button>();
-
-            // buttonNo.gameObject.SetActive(true);
-            // buttonYes.gameObject.SetActive(true);
             scene1.gameObject.SetActive(true);
         }
         else if ((int)number == 1)
@@ -84,20 +79,20 @@ public class RandomScene : MonoBehaviour
         }
     } 
 
-    public void whenYes()
+    public void whenYes1()
     {
         buttonToDiasable1 = GameObject.Find("Scenario1Yes").GetComponent<Button>();
         buttonToDiasable2 = GameObject.Find("Scenario1No").GetComponent<Button>();
         buttonToDiasable1.enabled = false;
         buttonToDiasable2.enabled = false;
-        scenarioText.text = "Studying is important, but you also need to have enough money to buy food. You trade potential study time for extra cash <br>  +15 Money <br> -10 Energy";
+        scenarioText.text = "Studying is important, but you also need to have enough money to buy food. You trade potential study time for extra cash <br>  +15 Money <br> -15 Energy";
         status.UpdateStats("money", 15);
-        status.UpdateStats("energy", -10);
+        status.UpdateStats("energy", -15);
         //continueButton.gameObject.SetActive(true);
         
     }
 
-    public void whenNo()
+    public void whenNo1()
     {
         buttonToDiasable1 = GameObject.Find("Scenario1Yes").GetComponent<Button>();
         buttonToDiasable2 = GameObject.Find("Scenario1No").GetComponent<Button>();
@@ -106,5 +101,62 @@ public class RandomScene : MonoBehaviour
         scenarioText.text = "You think about it and realize that studying is more important at the moment and that by valuing study time, is an even greater investment to your future.<br> +8 Money <br> -5 Energy";
         status.UpdateStats("money", 8);
         status.UpdateStats("energy", -5);
+    }
+
+    public void whenYes2()
+    {
+        buttonToDiasable1 = GameObject.Find("Scenario2Yes").GetComponent<Button>();
+        buttonToDiasable2 = GameObject.Find("Scenario2No").GetComponent<Button>();
+        buttonToDiasable1.enabled = false;
+        buttonToDiasable2.enabled = false;
+        scenarioText.text = "Work is just as important as school, you decide to help out your team and sacrifice time to study for that test.  <br>  +20 Money <br> -20 Energy <br> +7 Social";
+        status.UpdateStats("money", 20);
+        status.UpdateStats("energy", -20);
+        status.UpdateStats("social", +7);
+        
+    }
+
+    public void whenNo2()
+    {
+        buttonToDiasable1 = GameObject.Find("Scenario2Yes").GetComponent<Button>();
+        buttonToDiasable2 = GameObject.Find("Scenario2No").GetComponent<Button>();
+        buttonToDiasable1.enabled = false;
+        buttonToDiasable2.enabled = false;
+        scenarioText.text = "You really want to help, but picking up for a lost spot would come between time with your friends and studying, You decline helping out.<br> +5 Money <br> -3 Energy <br> +10 Social";
+        status.UpdateStats("money", 5);
+        status.UpdateStats("energy", -3);
+        status.UpdateStats("social", +10);
+    }
+
+    public void whenYes3()
+    {
+        buttonToDiasable1 = GameObject.Find("Scenario3Yes").GetComponent<Button>();
+        buttonToDiasable2 = GameObject.Find("Scenario3No").GetComponent<Button>();
+        buttonToDiasable1.enabled = false;
+        buttonToDiasable2.enabled = false;
+        scenarioText.text = "The manager rushes to help out! They commend you and you get a bonus for being an outstanding employee  <br>  +18 Money <br> -10 Energy <br> +12 Social";
+        status.UpdateStats("money", 18);
+        status.UpdateStats("energy", -10);
+        status.UpdateStats("social", +12);
+        
+    }
+
+    public void whenNo3()
+    {
+        buttonToDiasable1 = GameObject.Find("Scenario3Yes").GetComponent<Button>();
+        buttonToDiasable2 = GameObject.Find("Scenario3No").GetComponent<Button>();
+        buttonToDiasable1.enabled = false;
+        buttonToDiasable2.enabled = false;
+        scenarioText.text = "Your manager hears the commotion and rushes to see you getting beaten up pretty bad by the customer. You are sent home with docked pay and utter embarassment.<br> -20 Money <br> -20 Energy <br> -20 Social";
+        status.UpdateStats("money", -20);
+        status.UpdateStats("energy", -20);
+        status.UpdateStats("social", -20);
+    }
+
+    public void Default()
+    {
+        buttonToDiasable1 = GameObject.Find("Scenario4Yes").GetComponent<Button>();
+        buttonToDiasable1.enabled = false;
+        scenarioText.text = "A fine day at work!<br> +8 Money <br> -5 Energy";
     }
 }
